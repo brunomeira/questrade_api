10e3211f11147a2dfe922375f39a6ac72410129d
Bruno Meira <goesmeira@gmail.com>
2017-02-11 12:07:18 -0500

    Add tests for market call module

---
+++ spec/questrade_api/modules/market_call_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+require 'questrade_api/modules/market_call'
+
+describe QuestradeApi::MarketCall do
+  class DummyClass
+    attr_accessor :authorization
+
+    def initialize(authorization)
+      @authorization = authorization
+    end
+  end
+
+  let(:authorization) do
+    OpenStruct.new(access_token: '123', url: 'test')
+  end
+
+  subject { DummyClass.new(authorization).extend(QuestradeApi::MarketCall) }
+
+  context '.markets' do
+    it 'calls proper endpoint' do
+      expect(QuestradeApi::REST::Market).to receive(:fetch).and_return([])
+      expect(subject.markets).to eq([])
+    end
+  end
+
+  context '.symbols' do
+    it 'calls proper endpoint' do
+      expect(QuestradeApi::REST::Symbol).to receive(:fetch).and_return([])
+      expect(subject.symbols('')).to eq([])
+    end
+  end
+
+  context '.search_symbols' do
+    it 'calls proper endpoint' do
+      expect(QuestradeApi::REST::Symbol).to receive(:search).and_return([])
+      expect(subject.search_symbols('')).to eq([])
+    end
+  end
+
+  context '.quotes' do
+    it 'calls proper endpoint' do
+      expect(QuestradeApi::REST::Quote).to receive(:fetch).and_return([])
+      expect(subject.quotes([123])).to eq([])
+    end
+  end
+
+  context '.quote' do
+    it 'calls proper endpoint' do
+      expect_any_instance_of(QuestradeApi::REST::Quote).to receive(:fetch).and_return([])
+      expect(subject.quote(1)).to be_a(QuestradeApi::REST::Quote)
+    end
+  end
+
+  context '.candles' do
+    it 'calls proper endpoint' do
+      expect(QuestradeApi::REST::Candle).to receive(:fetch).and_return([])
+      expect(subject.candles('', '12')).to eq([])
+    end
+  end
+
+  context '.symbol_options' do
+    it 'calls proper endpoint' do
+      expect(QuestradeApi::REST::Market).to receive(:fetch).and_return([])
+      expect(subject.markets).to eq([])
+    end
+  end
+end
